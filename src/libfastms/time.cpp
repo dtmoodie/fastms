727e22cec91fc32676e5a1c0a4ccf35ef171b62a
Dan <dtmoodie>
2015-06-08 15:23:52 -0400

    Fixed bug with windows port. Made it output separately named library files for debug / release.

--- src/libfastms/time.cpp
+++ src/libfastms/time.cpp
@@ -22,14 +22,23 @@ int gettimeofday(struct timeval2 *tv/*in*/, struct timezone2 *tz/*in*/)
 	/*converting file time to unix epoch*/
 	tmpres /= 10;  /*convert into microseconds*/
 	tmpres -= DELTA_EPOCH_IN_MICROSECS;
-	tv->tv_sec = (__int32)(tmpres*0.000001);
-	tv->tv_usec = (tmpres % 1000000);
+	if (tv)
+	{
+		tv->tv_sec = (__int32)(tmpres*0.000001);
+		tv->tv_usec = (tmpres % 1000000);
+	}
+	
 
 
 	//_tzset(),don't work properly, so we use GetTimeZoneInformation
+	
 	rez = GetTimeZoneInformation(&tz_winapi);
-	tz->tz_dsttime = (rez == 2) ? true : false;
-	tz->tz_minuteswest = tz_winapi.Bias + ((rez == 2) ? tz_winapi.DaylightBias : 0);
+	if (tz)
+	{
+		tz->tz_dsttime = (rez == 2) ? true : false;
+		tz->tz_minuteswest = tz_winapi.Bias + ((rez == 2) ? tz_winapi.DaylightBias : 0);
+	}
+	
 
 	return 0;
 }
